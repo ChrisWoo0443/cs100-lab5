f4beb26c9ea095f1a306e474af90b57868f4be26
ChrisWoo0443 <chefwoodini@gmail.com>
2024-02-23 16:07:23 -0800

    Added CmakeLists.txt and updated tests.cpp

--- tests/test.cpp
+++ tests/test.cpp
@@ -11,6 +11,13 @@ TEST(RectangleTestSuite, TestConstructorSameNumbers) {
     ASSERT_EQ(2, r.get_width());
     ASSERT_EQ(2, r.get_height());
 }
+
+TEST(RectangleTestSuite, TestConstructorBigNumnbers) {
+    Rectangle r(1000, 500);
+    ASSERT_EQ(1000, r.get_width());
+    ASSERT_EQ(500, r.get_height());
+}
+
 TEST(RectangleTestSuite, TestAreaDiffNumbers) {
     Rectangle r(5, 6);
     ASSERT_EQ(30, r.area());
@@ -19,6 +26,11 @@ TEST(RectangleTestSuite, TestAreaSameNumbers) {
     Rectangle r(5, 5);
     ASSERT_EQ(25, r.area());
 }
+TEST(RectangleTestSuite, TestAreaBigNumber) {
+    Rectangle r(1500, 300);
+    ASSERT_EQ(450000, r.area());
+}
+
 TEST(RectangleTestSuite, TestPerimeterDifferentNumbers) {
     Rectangle r(1, 2);
     ASSERT_EQ(6, r.perimeter());
@@ -26,6 +38,11 @@ TEST(RectangleTestSuite, TestPerimeterDifferentNumbers) {
 TEST(RectangleTestSuite, TestPerimeterSameNumbers) {
     Rectangle r(10, 10);
     ASSERT_EQ(40, r.perimeter());
+}
+
+TEST(RectangleTestSuite, TestPerimeterBigNumbers){
+    Rectangle r(1200, 500);
+    ASSERT_EQ(3400, r.perimeter());
 }
 
 int main(int argc, char **argv) {
